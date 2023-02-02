8c3a62f329b970899cb3d987ba4b60469ac03aba
Jeronimas Dargis <jeronimasd@gmail.com>
2023-02-02 14:37:02 +0100

    feat: listening on hover event + creating an issue using linear API

--- src/extension.ts
+++ src/extension.ts
@@ -1,63 +1,89 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { LinearClient } from '@linear/sdk'
+import { LinearClient } from '@linear/sdk';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 
+// Place to user config
+const projectId = ''
 const client1 = new LinearClient({
-	apiKey: 'lin_api_kS0mGj8F6PjzfECCnSf5EaOZWux2AP66NfxlcDnC',
+	apiKey: '',
   });
   
 
-  client1.viewer.then(me => {
-	return me.assignedIssues().then(myIssues => {
-	  if (myIssues.nodes.length) {
-		myIssues.nodes.map(issue => console.log(`${me.displayName} has issue: ${issue.title}`));
-	  } else {
-		console.log(`${me.displayName} has no issues`);
-	  }
-	});
-  });
+//   client1.viewer.then(me => {
+// 	return me.assignedIssues().then(myIssues => {
+// 	  if (myIssues.nodes.length) {
+// 		myIssues.nodes.map(issue => console.log(`${me.displayName} has issue: ${issue.title}`));
+// 	  } else {
+// 		console.log(`${me.displayName} has no issues`);
+// 	  }
+// 	});
+//   });
 
-async function createIssue() {
+async function createIssue(parsedText: String) {
 	const teams = await client1.teams();
-	const projects = await client1.projects()
-	const gdaProject = projects.nodes[0];
-	const RCCOTeam = teams.nodes[0];
-  if (RCCOTeam.id) {
-	await client1.issueCreate({ projectId: gdaProject.id ,teamId: RCCOTeam.id, title: "My Created Issue" });
+	const projects = await client1.projects();
+	const selectedProject = projects.nodes.find((project) => project.slugId === projectId) || {id: ''};
+	const RCCO_TEAM = teams.nodes[0];
+
+  if (RCCO_TEAM.id) {
+	await client1.createIssue({ projectId: selectedProject.id ,teamId: RCCO_TEAM.id, title: `[TODO]: ${parsedText}` });
   }
   }
 
-  
+ 
 
 export function activate(context: vscode.ExtensionContext) {
 
-	// Api key authentication
+	vscode.languages.registerHoverProvider(
+		'javascript',
+		new (class implements vscode.HoverProvider {
+		  provideHover(
+			_document: vscode.TextDocument,
+			_position: vscode.Position,
+			_token: vscode.CancellationToken
+		  ): vscode.ProviderResult<vscode.Hover> {
+			const commentCommandUri = vscode.Uri.parse(`command:todo-to-linear.createTask`);
+			const contents = new vscode.MarkdownString(`[Create Todo](${commentCommandUri})`);
+			// To enable command URIs in Markdown content, you must set the `isTrusted` flag.
+			// When creating trusted Markdown string, make sure to properly sanitize all the
+			// input content so that only expected command URIs can be executed
+			contents.isTrusted = true;
+	
+			return new vscode.Hover(contents);
+		  }
+		})()
+	  );
   
-  	createIssue();
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "todo-to-linear" is now active!');
+	vscode.workspace.onDidSaveTextDocument(() => {
+		console.log('Document Save Trigger');
+	});
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('todo-to-linear.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from Todo To Linear!');
 	});
 
+	let hoverListener = vscode.commands.registerCommand('todo-to-linear.createTask', () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		let editor = vscode.window.activeTextEditor || {selection: null};
+		const selection = editor.selection;
+		if (selection && !selection.isEmpty) {
+			const selectionRange = new vscode.Range(selection.start.line, selection.start.character, selection.end.line, selection.end.character);
+			const highlighted = editor.document.getText(selectionRange);
+			console.log(highlighted);
+		}
+		// createIssue();
+	});
+
+	context.subscriptions.push(hoverListener);
 	context.subscriptions.push(disposable);
 }
 
 
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
 
 
